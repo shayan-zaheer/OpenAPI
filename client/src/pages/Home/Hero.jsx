0cf6bf0477d9d0c2b100aa6affab33a7e76f75b3
Arham097 <arhamhasan70@gmail.com>
2025-02-15 23:32:32 +0500

    Hero section completed

--- client/src/pages/Home/Hero.jsx
+++ client/src/pages/Home/Hero.jsx
@@ -4,8 +4,8 @@ import HeroDetails from "./HeroDetails";
 
 const Hero = () => {
   return (
-    <div className=" w-8/12  h-96 relative mx-auto md:rounded-2xl  top-6">
-      <div className="w-full min-h-full flex items-center justify-center absolute inset-0 bg-cover bg-center  rounded-2xl">
+    <div className=" w-9/12  lg:h-[400px] relative mx-auto md:rounded-2xl  top-12 ">
+      <div className="w-full min-h-full flex items-center justify-center absolute inset-0 bg-cover bg-center blur-md  rounded-2xl ">
         <img
           src={"/HeroBackground.png"}
           alt="Gemstone"
