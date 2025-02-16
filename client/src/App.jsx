3e8f2e7dd6cd74a8ee3673668d4dba5d3db24922
Arham097 <arhamhasan70@gmail.com>
2025-02-16 10:30:58 +0500

    a little designed modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import BrowseApis from "./pages/Search/SearchAPI";
 import UpdateAPI from "./pages/APIs/UpdateAPI";
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
+import PageNotFound from "./pages/404/PageNotFound";
 
 const App = () => {
   return (
@@ -26,7 +27,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/allApis/:language?" element={<BrowseApis />} />
-        <Route path="*" element={<div>Not Found</div>} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       {/* <Footer /> */}
     </BrowserRouter>
--- client/src/pages/Search/SearchAPI.jsx
+++ client/src/pages/Search/SearchAPI.jsx
@@ -134,7 +134,7 @@ const BrowseApis = () => {
                 animate={{ opacity: 1, scale: 1 }}
                 whileHover={{ scale: 1.03 }}
                 transition={{ duration: 0.3 }}
-                className="bg-[#22252b] p-6 rounded-lg shadow-md cursor-pointer hover:shadow-xl transition "
+                className="bg-[#22252b] p-6 rounded-lg shadow-md cursor-pointer hover:shadow-xl transition lg:w-80 lg:h-36 flex-wrap"
               >
                 <h2 className="text-white text-xl font-semibold">{api.name}</h2>
                 <p className="text-gray-400 mt-2 text-sm">{api.description}</p>
