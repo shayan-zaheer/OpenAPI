1b992004b409afb7a6dad275d7ad41d3f345d28c
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2025-07-12 16:27:30 +0500

    [FIX]: Used frontend deployed link

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["open-api-iota-eosin.vercel.app", "http://localhost:5173"],
         credentials: true,
     })
 );
