3aca0a60818c97063b9f8d65bed2bba5a4d49739
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2025-07-12 16:34:00 +0500

    [FIX]: Fixed url - added HTTPs

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(
     cors({
-        origin: ["open-api-iota-eosin.vercel.app", "http://localhost:5173"],
+        origin: ["https://open-api-iota-eosin.vercel.app", "http://localhost:5173"],
         credentials: true,
     })
 );
